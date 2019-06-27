09d048a08401f45f646590e23e1006ad24f191e6
Charlie <charlieferris15@gmail.com>
2019-06-27 22:57:03 +0100

    comments

--- utils/redditComments.js
+++ utils/redditComments.js
@@ -1,4 +1,10 @@
+/**
+ * @deprecated Since version 1.0.
+ */
 exports.run = async function(gliese, message) {
+    console.warn("Calling deprecated function 'run' in redditComments.js");
+    
+    //a function to handle the redditComments command
     let msgDAT = message.content.split(" ");
 
     if(msgDAT.length < 2)
@@ -13,51 +19,70 @@ exports.run = async function(gliese, message) {
     exports.getData(gliese, link, msgDAT[2], message, false);
 };
 
+
+/**
+ * @deprecated Since version 1.0.
+ */
 let total_replies = 1;
 
+//This function manages the data for getting a reply/ comment
 exports.getData = async function(gliese, link, start, message, is_reply){
-
+    //get fetch
     const fetch = require('node-fetch');
-
+    
+    //get the json data at the link
     let res = await fetch(link+".json");
     res = await res.json();
+    //no data returned
     if(res.error === 404) {
-        return message.channel.send("I can't find anything for the sub r/" + link);
+        return message.channel.send("I can't find anything for " + link);
     }
-
+    
+    //comments exist, and theyre stored here
     res = res[1].data.children;
+    
     let postnum = 0;//Math.floor(Math.random()*res.length);
     if(start===0)
         return message.channel.send("This is the first reply.");
-
+    
+    //start is the comment number, starting from one.  since indexing starts at 0, we subtract 1
     if(start){
         postnum = Number(start) -1;
     }
     start = postnum;
-
-    // for(;postnum < start+total_replies; postnum++) {
-        let post;
-
+   let post;
+        
+    //comments and replies require different code
         if(is_reply){
+            //locate reply data
             post = res[0].data;
             post = post.replies.data;
+            
+            //check if the post exists
             if(!post)
                 return message.channel.send("No more replies in this thread.");
-
+            
+            //check if the post exists
             if(post.children.length < postnum+1)
                 return message.channel.send("No more replies in this thread.");
-
+            
+            //get the data
             post = post.children[postnum].data;
         }else{
+            //check if the post exists
             if (postnum >= res.length || postnum < 0)
                 return message.channel.send("No more replies on this thread.");
+            
+            //get the data
             post = res[postnum].data;
         }
-
+        
+    //extract comment data
         let gildings = post.gildings;
         let total_awards_received = post.total_awards_received;
 
         let embed;
+    //style it in an embedded message
         embed = getEmbed(post, false, postnum+1);
         if (total_awards_received > 0) {
             let sil = gildings['gid_1'];
@@ -70,6 +95,7 @@ exports.getData = async function(gliese, link, start, message, is_reply){
             if (!pla) pla = 0;
             embed.addField("Silver", sil, true).addField("Gold", gol, true).addField("Platinum", pla, true);
         }
+    //finalize, send and react with navigation buttons
         embed.addField("Parent Link", link);
         let m = await message.channel.send(embed);
         await m.react('⬅');
@@ -78,8 +104,9 @@ exports.getData = async function(gliese, link, start, message, is_reply){
     // }
 }
 
-
+//embed the data
 function getEmbed(post, flag, id) {
+    //init vars && extract the post data.
     let url = "https://www.reddit.com" + post.permalink;
 
     let content = post.body;
@@ -89,12 +116,15 @@ function getEmbed(post, flag, id) {
 
     let upvotes = post.ups;
 
+    //unused flag
     if (flag)
         return {content: content, poster: poster, url:url};
 
+    //embed the data
     const Discord = require('discord.js');
     let embed = new Discord.RichEmbed().setTitle("Comment ["+id+"]").setColor(0xFF0000).setAuthor(poster).setTimestamp(created).setDescription(content.length > 2000?content.slice(0, 1997)+"...":content).setURL(url).addField("Upvotes", upvotes, true);
-
+    
+    //add the number of replies
     if(!post.replies.data){
         embed.addField("Replies", "0", true);
         return embed;
